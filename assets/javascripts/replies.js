85ec6c580c355824d0ec980c4a053aecf77aca4c
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2019-03-30 17:25:28 -0400

    Hide quick reply wiki button when no replies are created

--- assets/javascripts/replies.js
+++ assets/javascripts/replies.js
@@ -1,33 +1,39 @@
-jsToolBar.prototype.elements.replies = {
+(function() {
+  if (redmine_quick_replies.length === 0) {
+    return;
+  }
+
+  jsToolBar.prototype.elements.replies = {
     type: 'button',
     title: 'Replies',
     fn: {
-        wiki: function() {
-            var editor = this,
-                dropdown = $('<ul style="position:absolute;"></ul>');
+      wiki: function() {
+        var editor = this,
+            dropdown = $('<ul style="position:absolute;"></ul>');
 
-            for (var i = 0; i < redmine_quick_replies.length; i++) {
-                var reply = redmine_quick_replies[i],
-                    li = $('<li></li>');
+        for (var i = 0; i < redmine_quick_replies.length; i++) {
+          var reply = redmine_quick_replies[i],
+            li = $('<li></li>');
 
-                li.data('reply', reply);
+          li.data('reply', reply);
 
-                li.text(reply.name).appendTo(dropdown).mousedown(function() {
-                    editor.encloseSelection($(this).data('reply').body);
-                });
-            }
-
-            dropdown.menu().width(200).position({
-                my: 'left top',
-                at: 'left bottom',
-                of: this.toolNodes.replies
-            });
-
-            $(document).on('mousedown', function() {
-                dropdown.remove();
-            });
-
-            $('body').append(dropdown);
+          li.text(reply.name).appendTo(dropdown).mousedown(function() {
+            editor.encloseSelection($(this).data('reply').body);
+          });
         }
+
+        dropdown.menu().width(200).position({
+          my: 'left top',
+          at: 'left bottom',
+          of: this.toolNodes.replies
+        });
+
+        $(document).on('mousedown', function() {
+          dropdown.remove();
+        });
+
+        $('body').append(dropdown);
+      }
     }
-};
+  };
+})();
