8c556f2e338fa529cb6faeb3d8ccc7f38ed6c8b6
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2019-08-07 11:35:34 -0400

    Add test methods helper to run tests on Redmine 3 and 4

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,24 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 
 # Enable project fixtures
 ActiveRecord::FixtureSet.create_fixtures(File.dirname(__FILE__) + '/fixtures/', [:replies])
+
+module RedmineQuickReplies
+  module TestHelper
+    def compatible_request(type, action, parameters = {})
+      return send(type, action, :params => parameters) if Rails.version >= '5.1'
+      send(type, action, parameters)
+    end
+
+    def compatible_xhr_request(type, action, parameters = {})
+      return send(type, action, :params => parameters, :xhr => true) if Rails.version >= '5.1'
+      xhr type, action, parameters
+    end
+
+    def compatible_api_request(type, action, parameters = {}, headers = {})
+      return send(type, action, :params => parameters, :headers => headers) if Redmine::VERSION.to_s >= '3.4'
+      send(type, action, parameters, headers)
+    end
+  end
+end
+
+include RedmineQuickReplies::TestHelper
