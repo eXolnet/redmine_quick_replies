a79e8ec3305b16ed02699d0067583f9259f0b2de
Patrick Gagnon-Renaud <pgrenaud@exolnet.com>
2021-02-11 17:44:05 -0500

    Fix indent

--- lib/redmine_quick_replies/patches/wiki_formatting_patch.rb
+++ lib/redmine_quick_replies/patches/wiki_formatting_patch.rb
@@ -11,19 +11,19 @@ module RedmineQuickReplies
         def heads_for_wiki_formatter
           super
 
-            return if @heads_for_wiki_redmine_quick_replies_included
+          return if @heads_for_wiki_redmine_quick_replies_included
 
-            content_for :header_tags do
-                replies = Reply.visible.sorted.to_json
+          content_for :header_tags do
+            replies = Reply.visible.sorted.to_json
 
-                o = javascript_tag("redmine_quick_replies = " + replies + ";")
-                o << javascript_include_tag('replies', :plugin => 'redmine_quick_replies')
-                o << stylesheet_link_tag('replies', :plugin => 'redmine_quick_replies')
-                o.html_safe
-            end
-
-            @heads_for_wiki_redmine_quick_replies_included = true
+            o = javascript_tag("redmine_quick_replies = " + replies + ";")
+            o << javascript_include_tag('replies', :plugin => 'redmine_quick_replies')
+            o << stylesheet_link_tag('replies', :plugin => 'redmine_quick_replies')
+            o.html_safe
           end
+
+          @heads_for_wiki_redmine_quick_replies_included = true
+        end
       end
     end
   end
